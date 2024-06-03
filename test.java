4453fc05f639b78c57728f8f1e41e2cef6d1eb4b
Pc156 <pc156742@gmail.com>
2024-06-03 16:54:01 +0530

    java file modified

--- test.java
+++ test.java
@@ -8,5 +8,7 @@ public class test {
         System.out.println("Hello World");
 
         System.out.println("Java core added to the system,the end of the work");
+
+        System.out.println("hello second commit");
     }
 }
